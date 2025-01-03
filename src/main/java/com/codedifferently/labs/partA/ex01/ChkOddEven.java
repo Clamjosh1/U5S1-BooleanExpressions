053fb0f5af79702be52e7822991bfbfe055d2c90
Clamjosh <Clamjosh1@users.noreply.github.com>
2025-01-03 13:48:34 -0500

    I finished ChkOddEven

--- src/main/java/com/codedifferently/labs/partA/ex01/ChkOddEven.java
+++ src/main/java/com/codedifferently/labs/partA/ex01/ChkOddEven.java
@@ -3,10 +3,14 @@ package com.codedifferently.labs.partA.ex01;
 import java.util.Scanner;
 
 public class ChkOddEven {
-    public static String oddEven(){
+    public static String oddEven(int num){
         String response = "";
 
-
+        if (num %2 == 0) {
+            response = "The num is even " + num;
+        }else {
+            response = "The num is odd " + num;
+        }
 
         return response;
     }
@@ -14,8 +18,17 @@ public class ChkOddEven {
 
         Scanner in = new Scanner(System.in);
         int num = 0;
-        System.out.println("Enter a number:");
+        System.out.println("Enter a number between 1 and 10:");
         num = in.nextInt();
 
+
+        if (num >= 1 && num <= 10) {
+            String result = oddEven(num);
+            System.out.println(result);
+        }else {
+            System.out.println("Please enter a number between 1 and 10.");
+        }
+
+        in.close();
     }
 }
